d8dae917c1ac60242a4cc7edface9b5c4fcc17e7
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-14 03:06:05 +0200

    updated index.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <span>
-          ["Lisbon" "Paris"]
           <li>Sydney</li>
           <li>San Francisco</li>
         </span>
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function WeatherSearch() {
-  return <form>submit</form>;
+  return;
 }
